0bf9d0397346ef26ee9d2eaf6afd21e853a6c39a
yenlinh000 <yenlinh.nc21@gmail.com>
2021-06-06 14:08:54 +0700

    Add files via upload

---
+++ demo/index.php
@@ -0,0 +1,22 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Three JS example</title>
+        <style>
+        body {
+            margin: 0;
+            
+        }
+        canvas {
+            width: 100%;
+            height: 100%;
+        }
+        </style>
+    </head>
+    <body>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js"></script>
+        <script src="main.js"></script>
+    </body>
+</html>
---
+++ demo/main.js
@@ -0,0 +1,26 @@
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000    
+);
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer,domElement);
+
+const geometry = new THREE.BoxGeometry( 2, 2, 2);
+const material = new THREE.MeshBasicMaterial( {color: 0x000ff} );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    renderer.render(scene, camera);
+}
+
